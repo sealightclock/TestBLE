c32c9fe8df2eaa25b83218887cb69f67fdbdaa37
Sealight Clock <chbebram@hotmail.com>
2024-11-10 20:02:43 -0800

    Added BleScanView.kt

--- app/src/main/java/com/example/jonathan/testble/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testble/MainActivity.kt
@@ -1,54 +1,23 @@
 package com.example.jonathan.testble
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jonathan.testble.ui.theme.TestBLETheme
+import androidx.annotation.RequiresApi
 
 private const val TAG = "TBLE: MainActivity"
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
 
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-
         setContent {
-            TestBLETheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "TestBLE",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            BleScanner()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestBLETheme {
-        Greeting("TestBLE")
     }
 }
